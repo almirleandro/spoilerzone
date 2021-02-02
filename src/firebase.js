f448bf3f83f40f679dfd55c7d61d8ea1dc345087
Almir Leandro <leandroalmm50@gmail.com>
2021-02-02 19:21:16 -0300

    Update config

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import firebase from "firebase/app";
 import "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK9kyzWbX0DkL3lSaYysc6QIFy5QrR2xw",
-  authDomain: "moviespoilersdb.firebaseapp.com",
-  projectId: "moviespoilersdb",
-  storageBucket: "moviespoilersdb.appspot.com",
-  messagingSenderId: "430921595614",
-  appId: "1:430921595614:web:afe11c5fe403a076e4e04f"
+  apiKey: process.env.REACT_APP_FIREABSE_KEY,
+  authDomain: process.env.REACT_APP_FIREABSE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREABSE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREABSE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREABSE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREABSE_MESSAGING_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
