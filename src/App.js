2e23e35172caf47f66e16b72df7be72791b6c188
Almir Leandro <leandroalmm50@gmail.com>
2021-02-04 15:38:39 -0300

    Optimize images, implement code splitting and fix design bug at homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
 import Footer from './components/Footer'
-
-import About from './components/About'
-import Catalogo from './components/Catalogo'
-import Filme from './components/Filme'
-import Search from './components/Search'
 
 import ScrollToTop from './ScrollToTop'
 
@@ -17,45 +11,43 @@ import {
   HashRouter
 } from "react-router-dom"
 
+const HeroSection = React.lazy(() => import('./components/HeroSection'));
+const About = React.lazy(() => import('./components/About'));
+const Catalogo = React.lazy(() => import('./components/Catalogo'));
+const Filme = React.lazy(() => import('./components/Filme'));
+const Search = React.lazy(() => import('./components/Search'));
+
 export default function App() {
 
   return (
     <HashRouter basename='/'>
       <ScrollToTop />
       <div className="App">
+        <Navbar />
         <Switch>
+          <Suspense fallback={<div style={{height: '93vh'}}></div>}>
+            <Route path='/' exact>
+              <HeroSection />
+            </Route>
 
-          <Route path='/' exact>
-            <Navbar />
-            <HeroSection />
-            <Footer />
-          </Route>
+            <Route path='/catalogo'>
+              <Catalogo />
+            </Route>
 
-          <Route path='/catalogo'>
-            <Navbar />
-            <Catalogo />
-            <Footer />
-          </Route>
+            <Route path='/sobre'>
+              <About />
+            </Route>
 
-          <Route path='/sobre'>
-            <Navbar />
-            <About />
-            <Footer />
-          </Route>
+            <Route path='/filme/:id'>
+              <Filme />
+            </Route>
 
-          <Route path='/filme/:id'>
-            <Navbar />
-            <Filme />
-            <Footer />
-          </Route>
-
-          <Route path='/search/:query'>
-            <Navbar />
-            <Search />
-            <Footer />
-          </Route>
-
+            <Route path='/search/:query'>
+              <Search />
+            </Route>
+          </Suspense>
         </Switch>
+        <Footer />
       </div>
     </HashRouter>
   );
